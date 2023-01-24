5c215939a1fe7657f54ca40216143bfc431c7833
Badhon7821 <bayozidbostamibadhon@gmail.com>
2023-01-24 07:57:02 -0800

    Question 1 answer included

---
+++ Q1.js
@@ -0,0 +1,43 @@
+let arr1 = [2, 3, 4, 5, 6, 1];
+let arr2 = [4, 5, 4, 5, 1, 2];
+
+function coherence(arr1, arr2) {
+  let i = 0,
+    j = 0,
+    k = 0,
+    arr = [];
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] === arr2[j]) {
+      i++;
+      j++;
+    }
+    arr[k++] = arr1[i++];
+    arr[k++] = arr2[j++];
+  }
+
+  while (arr1.length < arr2.length) {
+    arr[k++] = arr2[j++];
+  }
+  while (arr1.length > arr2.length) {
+    arr[k++] = arr1[i++];
+  }
+
+  return bubbleSort(arr);
+}
+
+function bubbleSort(arr) {
+  var len = arr.length;
+  for (var i = 0; i < len; i++) {
+    for (var j = 0; j < len; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(coherence(arr1, arr2));
